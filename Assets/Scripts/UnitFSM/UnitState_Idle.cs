546ff007d8bddfcc813150d5b731a1c62b8f0967
a <a@a.a>
2020-02-01 19:30:35 +0900

    attack

--- Assets/Scripts/UnitFSM/UnitState_Idle.cs
+++ Assets/Scripts/UnitFSM/UnitState_Idle.cs
@@ -21,10 +21,19 @@ public class UnitState_Idle : AUnitState
             return unitStates[(int)Types.UnitFSMType.Move];
         }
 
-        //공격대상이 없으면 찾는다.
+        // 공격대상이 없으면 찾는다
         if (unit.AttackTargetUnit == null && unit.FindAttackTarget2() != null)
         {
+            // 공격대상을 찾았다
+            // todo 근거리 공격이면 이동, 원거리이면 공격
             Debug.Log("Found AttackTarget " + unit.AttackTargetUnit);
+            // 공격대상의 앞까지 이동할 waypoint 설정
+            unit.TargetWaypoint2 = WaypointManager.Inst.WaypointPool.Get();
+            unit.TargetWaypoint2.transform.position = unit.AttackTargetUnit.transform.position
+                + new Vector3(unit.UnitSize.x * 0.5f, 0.0f, 0.0f)
+                + new Vector3(unit.AttackTargetUnit.UnitSize.x * 0.5f, 0.0f, 0.0f);
+            unit.AttackTargetUnit.Notify(Types.UnitNotifyType.Wait, unit);
+            return unitStates[(int)Types.UnitFSMType.Move];
         }
 
         //공격대상이 있다
@@ -37,26 +46,26 @@ public class UnitState_Idle : AUnitState
                 return unitStates[(int)Types.UnitFSMType.Attack];
             }
             // 공격범위에 없으면 이동
-            else
-            {
-                Debug.Log("Move AttackTargetUnit " + unit.AttackTargetUnit);
-                if (unit.TargetWaypoint2 == null)
-                {
-                    unit.TargetWaypoint2 = WaypointManager.Inst.WaypointPool.Get();
-                    // 공격대상의 앞까지 이동할 waypoint 설정
-                    unit.TargetWaypoint2.transform.position = unit.AttackTargetUnit.transform.position
-                        + new Vector3(unit.UnitSize.x * 0.5f, 0.0f, 0.0f)
-                        + new Vector3(unit.AttackTargetUnit.UnitSize.x * 0.5f, 0.0f, 0.0f);
-                    // 공격대상에게 대기하라고 통보
-                    unit.AttackTargetUnit.Notify(Types.UnitNotifyType.Wait, unit);
-                }
-                else
-                {
-                    Debug.LogAssertion("unit.TargetWaypoint2 != null !!!");
-                }
+            //else
+            //{
+            //    Debug.Log("Move AttackTargetUnit " + unit.AttackTargetUnit);
+            //    if (unit.TargetWaypoint2 == null)
+            //    {
+            //        //unit.TargetWaypoint2 = WaypointManager.Inst.WaypointPool.Get();
+            //        //// 공격대상의 앞까지 이동할 waypoint 설정
+            //        //unit.TargetWaypoint2.transform.position = unit.AttackTargetUnit.transform.position
+            //        //    + new Vector3(unit.UnitSize.x * 0.5f, 0.0f, 0.0f)
+            //        //    + new Vector3(unit.AttackTargetUnit.UnitSize.x * 0.5f, 0.0f, 0.0f);
+            //        //// 공격대상에게 대기하라고 통보
+            //        //unit.AttackTargetUnit.Notify(Types.UnitNotifyType.Wait, unit);
+            //    }
+            //    else
+            //    {
+            //        Debug.LogAssertion("unit.TargetWaypoint2 != null !!!");
+            //    }
 
-                return unitStates[(int)Types.UnitFSMType.Move];
-            }
+            //    return unitStates[(int)Types.UnitFSMType.Move];
+            //}
         }
 
         // 대기장소로 이동
