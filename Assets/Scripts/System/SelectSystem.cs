ea90f13f6977acfa1079b46cea62c1cbfa7a9050
a <a@a.a>
2020-02-10 16:36:35 +0900

    click on waymap

--- Assets/Scripts/System/SelectSystem.cs
+++ Assets/Scripts/System/SelectSystem.cs
@@ -12,13 +12,17 @@ public class SelectSystem : SingletonBase<SelectSystem> {
     public Animator ClickCursorAnimator_Fail;
     public Animator ClickCursorAnimator_Success;
     public Animator RallyPointAnimator;
+    public SpriteRenderer MapSpriteRenderer;
     public SpriteRenderer WaymapSpriteRenderer;
+    public Vector3 _waymapSpritePivot;
 
     private int _pointerId;
 
     protected override void Awake()
     {
         base.Awake();
+
+        _waymapSpritePivot = new Vector3(WaymapSpriteRenderer.sprite.pivot.x, WaymapSpriteRenderer.sprite.pivot.y, 0f);
 
         //PC에서는 포인터 아이디가 -1이고 모바일에서는 0이므로 다르게 값을 넣어준다
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITYPLAYER
@@ -71,13 +75,14 @@ public class SelectSystem : SingletonBase<SelectSystem> {
                     SelectSelector(selector);
                 }
 
-                // 길을 클릭했는지 확인
-                Color pixel = WaymapSpriteRenderer.sprite.texture.GetPixel((int)Input.mousePosition.x, (int)Input.mousePosition.y);
+                // 클릭한 위치의 Waymap의 색상정보로 길인지 아닌지 판단
+                // Map의 월드에서의 Map의 위치와 스케일까지 고려
+                Vector3 positionOnWaymap = (_waymapSpritePivot + ((clickPosition - MapSpriteRenderer.transform.position) * WaymapSpriteRenderer.sprite.pixelsPerUnit / MapSpriteRenderer.gameObject.transform.localScale.x));
+                Color pixelOnWaymap = WaymapSpriteRenderer.sprite.texture.GetPixel((int)positionOnWaymap.x, (int)positionOnWaymap.y);
+
                 clickPosition.z = 0;
                 ClickContainer.transform.position = clickPosition;
-                Debug.Log("Click color " + pixel);
-
-                if(pixel == Color.black) // 길 클릭
+                if(pixelOnWaymap == Color.black) // 길 클릭
                 {
                     ClickCursorAnimator_Success.gameObject.SetActive(true);
                     ClickCursorAnimator_Fail.gameObject.SetActive(false);
@@ -90,6 +95,8 @@ public class SelectSystem : SingletonBase<SelectSystem> {
                     ClickCursorAnimator_Fail.SetTrigger("Click");
                 }
 
+                // 현재 Barraks의 랠리포인트 지정하고 있으면 RallyPointFlag 출력
+                // todo rally Point mode
                 if (true)
                 {
                     RallyPointAnimator.gameObject.SetActive(true);
@@ -98,10 +105,6 @@ public class SelectSystem : SingletonBase<SelectSystem> {
                 {
                     RallyPointAnimator.gameObject.SetActive(false);
                 }
-
-                // todo 클릭 결과에 따라 클릭 아이콘 애니 재생 출력
-                //Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //ClickCursorAnimator_Success.SetTrigger("Click");
             }
         }
     }
