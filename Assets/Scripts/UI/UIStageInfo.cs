4cc7399f928d43eda848270e4fcf076f76e771f7
a <a@a.a>
2020-02-28 12:57:05 +0900

    zoom button

--- Assets/Scripts/UI/UIStageInfo.cs
+++ Assets/Scripts/UI/UIStageInfo.cs
@@ -3,32 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIStageInfo : MonoBehaviour {
+public class UIStageInfo : MonoBehaviour
+{
     public Text HealthText;
     public Text GoldText;
     public Text WaveText;
     public Button StartWaveButton;
+    public Button ZoomButton;
+    public bool IsZoomIn = true;
 
     StageData _stageData;
     StageInfo _stageInfo;
 
-    void Start () {
+    void Start()
+    {
         _stageData = StageManager.Inst.StageData;
         _stageInfo = StageManager.Inst.StageInfo;
         StageManager.Inst.StageEvent += OnStageEvent;
 
-        StartWaveButton.onClick.AddListener(() => {
+        StartWaveButton.onClick.AddListener(() =>
+        {
             StageManager.Inst.RunWave();
         });
 
-        _UpdateStageInfo();
+        ZoomButton.onClick.AddListener(() =>
+        {
+            ToggleZoomMap();
+        });
+        ToggleZoomMap();
+
+        UpdateStageInfo();
     }
-	
-    void _UpdateStageInfo()
+
+    void ToggleZoomMap()
+    {
+        InputManager.Inst.InputEvent(Types.InputEventType.Zoom, new Vector3(IsZoomIn ? 1f : -1f, 0f, 0f));
+        IsZoomIn = !IsZoomIn;
+        ZoomButton.GetComponentInChildren<Text>().text = IsZoomIn ? "지도 확대" : "지도 축소";// todo translate
+    }
+
+    void UpdateStageInfo()
     {
         HealthText.text = _stageInfo.Health.ToString();
         GoldText.text = _stageInfo.Gold.ToString();
-        WaveText.text = "공격 " + (_stageInfo.WavePhaseIndex + 1).ToString() + "/" + StageManager.Inst.StageInfo.WavePhaseCount.ToString();
+        WaveText.text = "공격 " + (_stageInfo.WavePhaseIndex + 1).ToString() + "/" + StageManager.Inst.StageInfo.WavePhaseCount.ToString();// todo translate
     }
 
     public void OnStageEvent(Types.StageEventType stageEventType)
@@ -38,7 +56,7 @@ public class UIStageInfo : MonoBehaviour {
             case Types.StageEventType.None:
                 break;
             case Types.StageEventType.StageInfoChanged:
-                _UpdateStageInfo();
+                UpdateStageInfo();
                 break;
             default:
                 break;
